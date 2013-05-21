888af132a0b2544ee4376c1e032fca0be1bc59bb
=greengerong <=grzx2210@163.com>
2013-05-21 20:00:53 +0800

    page for show monitor

--- src/main/java/green/monitor/controller/MonitorController.java
+++ src/main/java/green/monitor/controller/MonitorController.java
@@ -1,8 +1,10 @@
 package green.monitor.controller;
 
+import com.ibm.wsdl.util.IOUtils;
 import green.monitor.IMonitorFactory;
 import green.monitor.MonitorResult;
 import green.monitor.Monitoring;
+import green.monitor.ResourceUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.util.Map;
 
 @Controller
@@ -24,23 +30,35 @@ public class MonitorController {
     }
 
     @RequestMapping(value = "config", method = RequestMethod.GET)
-    public
     @ResponseBody
-    Monitoring getConfig() throws Exception {
+    public Monitoring getConfig() throws Exception {
         return monitorFactory.getMonitoring();
     }
 
     @RequestMapping(value = "{id}", method = RequestMethod.GET)
-    public
     @ResponseBody
-    MonitorResult run(@PathVariable String id) throws Exception {
+    public MonitorResult run(@PathVariable String id) throws Exception {
         return monitorFactory.run(id);
     }
 
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public
     @ResponseBody
-    Map<String, MonitorResult> runAll() throws Exception {
+    public Map<String, MonitorResult> runAll() throws Exception {
         return monitorFactory.runAll();
+    }
+
+    @RequestMapping(value = "resource/{type}/{template}", method = RequestMethod.GET)
+    @ResponseBody
+    public String getResource(@PathVariable String type, @PathVariable String template) throws IOException {
+        InputStream inputStream = null;
+        try {
+            System.out.println(template);
+            inputStream = ResourceUtil.getCurrentResourceAsStream(String.format("%s.%s", template, type));
+            return IOUtils.getStringFromReader(new InputStreamReader(inputStream));
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
     }
 }
---
+++ src/main/resources/monitor.js
@@ -0,0 +1,4 @@
+var monitorApp = angular.module("monitorApp", [])
+    .controller("monitorCtr", ["$timeout", function ($timeout) {
+
+    }]);
